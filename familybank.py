cbaa710c91f761fe4ddb387dd740f4f0f0888c2c
Matt Riley <spam@oscmp.com>
2016-02-28 21:59:00 -0500

    Add basic sb admin template.

--- familybank.py
+++ familybank.py
@@ -4,13 +4,24 @@ from web_plugins.app import application
 from web_plugins.response import HtmlResponse
 import web_plugins.router as r
 
+from web_plugins.response import HtmlTemplateResponse
+import web_plugins.htmlpage as wp
+import web_plugins.template as t
+
+
 def familybank(request):
+	response = HtmlTemplateResponse('dashboard.mustache')
+	response.arguments = {}
+	return response
+"""
 	response = HtmlResponse()
 	response.response_text = "familybank feels great."
 	return response
+"""
 
 static_router = r.FileRoute('/','./static')
 router = r.FirstMatchRouter()
 router.routes.extend([static_router, r.Route(familybank)])
 application.handler = router
 
+HtmlTemplateResponse.default_template_handler = t.TemplateHandler(t.PystacheFileAdapter('./templates'))
