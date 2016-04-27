519d4820967a537bffaf137b372d01a0176c4e47
Matt Riley <spam@oscmp.com>
2016-04-27 02:05:21 +0000

    add dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,8 @@ pip install virtualenv
 virtualenv virtual_env
 source virtual_env/bin/activate
 pip install git+ssh://git@github.com/rileymat/web_plugins.git
+sudo apt-get install libffi-dev
+pip install bcrypt
+pip install pystache
 setup_app.py --no-generate-basic-site --no-create-git-repo --app-name=familybank
 
